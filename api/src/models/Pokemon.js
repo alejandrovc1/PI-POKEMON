0de05d474902fb311c33e5a901e8c5a45d174176
alejandrovc1 <alejandrovc-@outlook.com>
2022-08-30 20:43:27 -0500

    Backend v1.0

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,39 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    life: {
+      type: DataTypes.STRING,
+    },
+    attack: {
+      type: DataTypes.STRING,
+    },
+    defense: {
+      type: DataTypes.STRING,
+    },
+    speed: {
+      type: DataTypes.STRING,
+    },
+    height: {
+      type: DataTypes.STRING,
+    },
+    weight: {
+      type: DataTypes.STRING,
+    },
+    origin: {
+      type: DataTypes.STRING,
+    },
+    img: {
+      type: DataTypes.STRING,
+      defaultValue: "https://static.wikia.nocookie.net/eswikia/images/d/df/Pok%C3%A9mon.png/revision/latest/scale-to-width-down/980?cb=20170308220152"
+    }
+  }, { timestaps: false });
 };
